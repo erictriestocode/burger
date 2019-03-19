914087108f53b041a57a47c4a876102d24ad666e
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-18 22:43:21 -0500

    began constructing router file

---
+++ controllers/burgerflipper.js
@@ -0,0 +1,12 @@
+// Controller file for Burger App routes
+// *********************************************
+// Bring in Express and create a variable for it
+const express = require("express");
+const router = express.Router();
+
+// TODO: Burger model(Inspired by MVC assignment) 
+const burger = require("Path to burger model file");
+
+//ROUTES!
+
+
