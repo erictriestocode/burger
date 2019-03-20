905a2ba127d30e72eb4b8a7f96c6066dce1e6384
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-19 22:33:37 -0500

    Seeds Creation

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+-- Inspired by Kumas Corner
+-- https://kumascorner.com/#menu
+INSERT INTO burgers (burger_name,devoured) VALUES ("The Famous Kuma", true);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Kaijo", false);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Slayer", true);
+INSERT INTO burgers (burger_name,devoured) VALUES ("Sourvein", false);
