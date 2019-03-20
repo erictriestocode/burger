3b2c2d903fd44e1b64e9c7766b00d25f2ce0834c
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-03-19 23:35:59 -0500

    burger models set hopefully? I'm braindead.

--- models/burger.js
+++ models/burger.js
@@ -3,28 +3,23 @@
 
 let orm = require("../config/orm.js");
 
-let cat = {
-    // all: function(cb){
-    //     orm.all("burgers", cols, vals, function(res){
-    //         cb(res);
-    //     });
-    // },
+let burger = {
+    all: function(cb){
+        orm.all("burgers", cols, vals, function(res){
+            cb(res);
+        });
+    },
     create: function(cols, vals, cb){
         orm.create("burgers", cols, vals, function(res){
             cb(res);
         });
     },
-
+    update: function(objColVals,condition,cb){
+        orm.update("burgers", objColVals, condition, function(res){
+            cb(res);
+        });
+    }
     
-
-
-
-
-
-
-
-
-
-
-
 }
+
+module.exports = burger;
